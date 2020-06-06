51413e3bb90d3c5f567f3d5fe90b2d76e1cbd124
GGolfz <wisarutgolf11@gmail.com>
2020-06-06 18:55:46 +0700

    [Create] Khaibook Project  & [ADD] Navbar

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar color="#CEAB7E" app>
+        <img src="../assets/icon.png" height="54px"/>
+      <v-toolbar-title class="display-1 white--text shoptitle">
+        Khaibook
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-app-bar-nav-icon>
+      </v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-content>
+    </v-content>
+  </v-app>
+</template>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@200;400&display=swap');
+
+.v-application{
+  font-family: 'Kanit', sans-serif !important;
+}
+.shoptitle{
+  text-shadow: 0px 2.80183px 2.80183px rgba(0, 0, 0, 0.25);
+}
+</style>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    Hello
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
